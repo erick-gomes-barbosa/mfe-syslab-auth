aa17002493fddb52c4d36165aaf6c31f4fb5d3dc
erickWillyan-Baymetrics <erick.cruz@baymetrics.com.br>
2025-06-13 19:59:06 -0300

    Fix: Resolves Prettier and Husky bugs

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -30,7 +30,7 @@ export default function AuthPage() {
   };
 
   const submitForm = (data) => {
-    console.log(data);
+    // console.log(data);
   };
   return (
     <>
@@ -38,43 +38,82 @@ export default function AuthPage() {
         <img src={SyslabLogo} alt="SyslabLogo" />
       </div>
 
-      <div className="">
-        <form
-          onClick={handleSubmit(submitForm)}
-          className="flex flex-col rounded-4xl bg-[#2C5B8C26] h-11/12 w-11/12 m-auto pb-7 overflow-hidden "
-        >
-          <div className="flex w-full font-bold mb-4 ">
-            <button
-              onClick={setSelectButtonColor}
-              className={`h-11 w-1/2 text-[#333333] ${
-                isloginComponent && "bg-[#F0F4F8] rounded-br-lg"
-              }`}
-            >
-              Cadastro
-            </button>
-            <button
-              onClick={setSelectButtonColor}
-              className={`h-11 w-1/2 text-[#333333] ${
-                isloginComponent ? "" : "bg-[#F0F4F8] rounded-bl-lg"
-              }`}
-            >
-              Login
-            </button>
-          </div>
+      <form
+        onSubmit={handleSubmit(submitForm)}
+        className="flex flex-col rounded-4xl bg-[#2C5B8C26] h-11/12 w-11/12 m-auto pb-7 overflow-hidden "
+      >
+        <div className="flex w-full font-bold mb-4 ">
+          <button
+            onClick={setSelectButtonColor}
+            className={`h-11 w-1/2 text-[#333333] ${
+              isloginComponent && "bg-[#F0F4F8] rounded-br-lg"
+            }`}
+          >
+            Cadastro
+          </button>
+          <button
+            onClick={setSelectButtonColor}
+            className={`h-11 w-1/2 text-[#333333] ${
+              isloginComponent ? "" : "bg-[#F0F4F8] rounded-bl-lg"
+            }`}
+          >
+            Login
+          </button>
+        </div>
 
-          {/* Renderiza um componente com base se está no formulário de login ou não */}
-          {isloginComponent ? (
-            <>
-              <div className="w-10/12 m-auto mb-4">
+        {/* Renderiza um componente com base se está no formulário de login ou não */}
+        {isloginComponent ? (
+          <>
+            <div className="w-10/12 m-auto mb-4">
+              <LabelForm title="E-mail:" />
+              <TextboxInput
+                name="email"
+                required={true}
+                type="email"
+                register={register}
+              />
+            </div>
+            <div className="w-10/12 m-auto mb-4">
+              <LabelForm title="Senha:" />
+              <TextboxInput
+                name="password"
+                required={true}
+                type="password"
+                register={register}
+              />
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="flex flex-col w-10/12 m-auto mb-4 gap-4">
+              <div>
+                <LabelForm title="Matrícula:" />
+                <TextboxInput
+                  name="registery"
+                  required={true}
+                  type="text"
+                  register={register}
+                />
+              </div>
+              <div>
+                <LabelForm title="Nome Completo:" />
+                <TextboxInput
+                  name="name"
+                  required={true}
+                  type="text"
+                  register={register}
+                />
+              </div>
+              <div>
                 <LabelForm title="E-mail:" />
                 <TextboxInput
                   name="email"
                   required={true}
-                  type="email"
+                  type="text"
                   register={register}
                 />
               </div>
-              <div className="w-10/12 m-auto mb-4">
+              <div>
                 <LabelForm title="Senha:" />
                 <TextboxInput
                   name="password"
@@ -83,83 +122,42 @@ export default function AuthPage() {
                   register={register}
                 />
               </div>
-            </>
-          ) : (
-            <>
-              <div className="flex flex-col w-10/12 m-auto mb-4 gap-4">
-                <div>
-                  <LabelForm title="Matrícula:" />
-                  <TextboxInput
-                    name="registery"
-                    required={true}
-                    type="text"
-                    register={register}
-                  />
-                </div>
-                <div>
-                  <LabelForm title="Nome Completo:" />
-                  <TextboxInput
-                    name="name"
-                    required={true}
-                    type="text"
-                    register={register}
-                  />
-                </div>
-                <div>
-                  <LabelForm title="E-mail:" />
-                  <TextboxInput
-                    name="email"
-                    required={true}
-                    type="text"
-                    register={register}
-                  />
-                </div>
-                <div>
-                  <LabelForm title="Senha:" />
-                  <TextboxInput
-                    name="password"
-                    required={true}
-                    type="password"
-                    register={register}
-                  />
-                </div>
 
-                <div>
-                  <LabelForm title="Ocupação:" />
-                  <InputSelect
-                    name="type"
-                    options={optionsOcupation}
+              <div>
+                <LabelForm title="Ocupação:" />
+                <InputSelect
+                  name="type"
+                  options={optionsOcupation}
+                  required={true}
+                  register={register}
+                  defaultValue={null}
+                />
+              </div>
+              <div className="flex justify-between">
+                <div className="w-[47.5%]">
+                  <LabelForm title="Horário de Entrada:" />
+                  <InputTime
+                    name="entry_time"
                     required={true}
                     register={register}
-                    defaultValue={null}
                   />
                 </div>
-                <div className="flex justify-between">
-                  <div className="w-[47.5%]">
-                    <LabelForm title="Horário de Entrada:" />
-                    <InputTime
-                      name="entry_time"
-                      required={true}
-                      register={register}
-                    />
-                  </div>
-                  <div className="w-[47.5%] \">
-                    <LabelForm title="Horário de Saida:" />
-                    <InputTime
-                      name="departure_time"
-                      required={true}
-                      register={register}
-                    />
-                  </div>
+                <div className="w-[47.5%] \">
+                  <LabelForm title="Horário de Saida:" />
+                  <InputTime
+                    name="departure_time"
+                    required={true}
+                    register={register}
+                  />
                 </div>
               </div>
-            </>
-          )}
-          <div className="flex justify-center">
-            <InputSubmit text={isloginComponent && "Login"} />
-          </div>
-        </form>
-      </div>
+            </div>
+          </>
+        )}
+        <div className="flex justify-center">
+          <InputSubmit text={isloginComponent && "Login"} />
+        </div>
+      </form>
     </>
   );
 }
